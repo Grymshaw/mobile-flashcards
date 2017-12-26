aacfe7ec1626edf55d943ce63164fe91b27cfe8d
Kyle Grymonpre <kgrymonpr@gmail.com>
2017-12-26 15:55:47 -0600

    Feat: Add push notifications for study reminders

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-// import NewDeck from './components/NewDeck'
 import StatusBar from './components/StatusBar'
 import Navigation from './components/Navigation'
+
+import { setLocalNotification } from './utils/helpers'
 
 const styles = StyleSheet.create({
   container: {
@@ -15,9 +16,14 @@ const styles = StyleSheet.create({
 });
 
 export default class App extends React.Component {
+  componentDidMount () {
+    setLocalNotification()
+  }
+
   onNavigationStateChange(prevState, newState) {
     this.setState({ ...this.state, routeIndex: newState.routes[0].index })
   }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
--- components/DeckDetails/index.js
+++ components/DeckDetails/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { View, Text, Button } from 'react-native'
 
+import styles from './styles'
 import { getDeck } from '../../utils/deckApi'
+import { clearLocalNotification, setLocalNotification } from '../../utils/helpers'
 
 export default class DeckDetails extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -13,7 +15,9 @@ export default class DeckDetails extends Component {
     this.state = {
       deck: {},
       loading: true,
+      showWarning: false, // show error if trying to start quiz with no questions
     }
+    this.handleQuizStart = this.handleQuizStart.bind(this)
   }
 
   componentDidMount () {
@@ -42,23 +46,52 @@ export default class DeckDetails extends Component {
     navigation.navigate(route, { deck })
   }
 
+  handleQuizStart () {
+    const { deck } = this.state
+    const { navigation } = this.props
+
+    if (deck.questions && deck.questions.length > 0) {
+      navigation.navigate('QuizView', { deck })
+      // clear current reminder notification and set again for tomorrow
+      clearLocalNotification()
+        .then(setLocalNotification)
+    } else {
+      this.setState({
+        showWarning: true,
+      })
+      // remove error message after 5 seconds
+      setTimeout(() => {
+        this.setState({
+          showWarning: false,
+        })
+      }, 5000)
+    }
+  }
+
   render() {
-    const { deck, loading } = this.state
+    const { deck, loading, showWarning } = this.state
 
     return loading
       ? <Text>Loading...</Text>
-      : <View>
-        <Text>
-          {deck.questions.length} question{deck.questions.length > 1 ? 's' : ''}
-        </Text>
-          <Button
-            title={'Add question'}
-            onPress={() => this.goTo('NewQuestion')}
-          />
-          <Button
-            title={'Start quiz'}
-            onPress={() => this.goTo('QuizView')}
-          />
+      : <View style={styles.mainContainer}>
+          <Text style={styles.questions}>
+            {deck.questions.length} Question{deck.questions.length !== 1 ? 's' : ''}
+          </Text>
+            <Button
+              title={'Add question'}
+              style={styles.addQuestion}
+              onPress={() => this.goTo('NewQuestion')}
+            />
+            <Button
+              title={'Start quiz'}
+              style={styles.startQuiz}
+              onPress={this.handleQuizStart}
+            />
+            {showWarning
+                ? <Text style={styles.warning}>
+                    Add some questions before starting the quiz!
+                  </Text>
+                : null}
         </View>
   }
 }
